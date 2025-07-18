75d333123e1ed3bebc84772c4212f719a896a04e
aijaz <saijazahmad38@gmail.com>
2025-07-18 19:43:47 +0530

    fixed login

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -3,46 +3,54 @@ import InputBox from "./InputBox"
 import axios from "axios"
 import { ApiConstants } from "../utils/ApiConstants"
 import { toast } from "react-toastify"
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
+import { Loader } from "lucide-react";
 
 const Auth = () => {
     const [isSigninForm, setIssignForm] = useState(false)
     const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
   const [name, setName] = useState("")
  const navigate = useNavigate()
   const handleSubmit = async () => {
     if (!isSigninForm) {
       try {
+        setLoading(true)
         const response = await axios.post(ApiConstants.SIGNUP, {
           email,
           password,
           name
         })
-       
+        setLoading(false)
         const jwt = response.data.jwt;
         localStorage.setItem("token", jwt)
+       
         toast.success("User registered successfully")
          navigate("/dashboard")
       } catch(error) {
-    
+         setLoading(false)
         //@ts-ignore
         return toast.error(error.response.data.message)
        }
     }
     if (isSigninForm) {
       try {
+        setLoading(true)
         const response = await axios.post(ApiConstants.SIGNIN, {
           email,
           password,
         })
+         setLoading(false)
         const jwt = response.data.token;
         localStorage.setItem("token", jwt)
-        console.log(response)
+       
         toast.success("logged in successfully")
         navigate("/dashboard")
       } catch (error) {
-        //@ts-ignore
+        
+        setLoading(false)
+         //@ts-ignore
         return toast.error(error.response.data.message)
     }
     }
@@ -52,10 +60,11 @@ const Auth = () => {
     setIssignForm(!isSigninForm)
   }
   return (
-    <div className="flex h-screen items-center justify-center  flex-col">
-      <div className="flex flex-col justify-center">
+    <div className="flex h-screen items-center justify-center  flex-col ">
+      <div className="items-center flex flex-col  boder-b-2 shadow-2xl p-5 rounded-lg ">
+         <div className="flex flex-col justify-center ">
          
-        <div className="text-3xl font-extrabold px-10">
+        <div className="text-3xl  font-extrabold px-10">
           {!isSigninForm ? "Create An Account ? " : "Login to existing account"}
            </div>
         
@@ -64,18 +73,21 @@ const Auth = () => {
         </div>
         
       </div>
-      <div className="w-1/2 mt-4 s">
+      <div className="w-full mt-4 p-4">
           <InputBox label="Email" placeholder="johndoe@gmail.com" type="text" onChange={(e) => setEmail(e.target.value)} />
           <InputBox label="Password" placeholder="********" type="password" onChange={(e) => setPassword(e.target.value)} />
           { !isSigninForm && <InputBox label="Name" placeholder="John Doe" type="text" onChange={(e) => setName(e.target.value)} />}
         <div className="px-2 mt-8 items-center ">
-           <button onClick={handleSubmit} className=" bg-gray-900 w-full text-white 
-           py-2 rounded-lg    ">
-            {!isSigninForm ? "Sign Up" : "Sign In"}
-          </button>
+           <button onClick={handleSubmit} className="bg-indigo-600 hover:bg-indigo-700 items-center w-full text-white 
+           py-2 rounded-lg flex justify-center  ">
+            {loading?<Loader className="animate-spin w-5 h-5"/>:(!isSigninForm ? "Sign Up" : "Sign In")}
+            </button>
+            
         </div>
        
       </div>
+      </div>
+     
          
      </div>     
    
--- frontend/src/components/InputBox.tsx
+++ frontend/src/components/InputBox.tsx
@@ -10,10 +10,10 @@ interface InputProps{
 const InputBox = ({label, placeholder, type, onChange}:InputProps) => {
   return (
       <div className="p-2">
-          <label className="block mb-2 text-lg font-bold text-gray-900 dark:text-white">{ label}</label>
-          <input onChange={onChange} type={type} id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
-             focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600
-              dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          <label className="block mb-2 text-lg font-bold text-black">{ label}</label>
+          <input onChange={onChange} type={type} id="first_name" className="bg-gray-50 border border-gray-300 text-black text-md rounded-lg
+              block w-full p-2.5 
+              "
               placeholder={placeholder} />
     </div>
   )
